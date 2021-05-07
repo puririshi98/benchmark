25e38cb47cf5afd07a91c1c8104a663267dfb450
riship <riship@nvidia.com>
2021-05-07 11:54:16 -0700

    nvtx profiling, cuda graphs, and nvfuser

--- conftest.py
+++ conftest.py
@@ -18,13 +18,13 @@ def pytest_addoption(parser):
                     help="Run benchmarks on cpu only and ignore machine configuration checks")
 
 def set_fuser(fuser):
-    torch._C._jit_set_nvfuser_enabled(True)
-    torch._C._jit_set_texpr_fuser_enabled(False)
-    torch._C._jit_set_profiling_executor(True)
+	torch._C._jit_set_profiling_executor(True)
     torch._C._jit_set_profiling_mode(True)
-    torch._C._jit_override_can_fuse_on_cpu(False)
-    torch._C._jit_override_can_fuse_on_gpu(False)
     torch._C._jit_set_bailout_depth(20)
+    torch._C._jit_set_num_profiled_runs(2)
+    torch._C._jit_override_can_fuse_on_cpu(False)
+    torch._C._jit_override_can_fuse_on_gpu(True)
+    torch._C._jit_set_texpr_fuser_enabled(True)
 
 
 def pytest_sessionstart(session):
