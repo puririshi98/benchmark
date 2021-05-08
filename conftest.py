a4df694abc4e1cf005c1eeab168bcb5e689e6f5c
riship <riship@nvidia.com>
2021-05-08 13:32:57 -0700

    nvtx profiling, cuda graphs, and nvfuser

--- conftest.py
+++ conftest.py
@@ -18,27 +18,22 @@ def pytest_addoption(parser):
                     help="Run benchmarks on cpu only and ignore machine configuration checks")
 
 def set_fuser(fuser):
-    if fuser == "old":
-        torch._C._jit_set_profiling_executor(False)
-        torch._C._jit_set_profiling_mode(False)
-        torch._C._jit_override_can_fuse_on_gpu(True)
-        torch._C._jit_set_texpr_fuser_enabled(False)
-    elif fuser == "te":
-        torch._C._jit_set_profiling_executor(True)
-        torch._C._jit_set_profiling_mode(True)
-        torch._C._jit_set_bailout_depth(20)
-        torch._C._jit_set_num_profiled_runs(2)
-        torch._C._jit_override_can_fuse_on_cpu(False)
-        torch._C._jit_override_can_fuse_on_gpu(True)
-        torch._C._jit_set_texpr_fuser_enabled(True)
-    else:
-        torch._C._jit_set_nvfuser_enabled(True)
-        torch._C._jit_set_texpr_fuser_enabled(False)
-        torch._C._jit_set_profiling_executor(True)
-        torch._C._jit_set_profiling_mode(True)
-        torch._C._jit_override_can_fuse_on_cpu(False)
-        torch._C._jit_override_can_fuse_on_gpu(False)
-        torch._C._jit_set_bailout_depth(20)
+    #te
+    # torch._C._jit_set_profiling_executor(True)
+    # torch._C._jit_set_profiling_mode(True)
+    # torch._C._jit_set_bailout_depth(20)
+    # torch._C._jit_set_num_profiled_runs(2)
+    # torch._C._jit_override_can_fuse_on_cpu(False)
+    # torch._C._jit_override_can_fuse_on_gpu(True)
+    # torch._C._jit_set_texpr_fuser_enabled(True)
+    #NVFuser
+    torch._C._jit_set_nvfuser_enabled(True)
+    torch._C._jit_set_texpr_fuser_enabled(False)
+    torch._C._jit_set_profiling_executor(True)
+    torch._C._jit_set_profiling_mode(True)
+    torch._C._jit_override_can_fuse_on_cpu(False)
+    torch._C._jit_override_can_fuse_on_gpu(False)
+    torch._C._jit_set_bailout_depth(20)
 
 
 
