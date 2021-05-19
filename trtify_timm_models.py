2b4285d6921259942a54f53ae4e79ebe989bd281
riship <riship@nvidia.com>
2021-05-19 06:20:12 -0700

    nvtx profiling, cuda graphs, and nvfuser

---
+++ trtify_timm_models.py
@@ -0,0 +1,35 @@
+from torchbenchmark import list_models
+import torch
+
+effnet=None
+vt=None
+for Model in list_models():
+	if 'timm_efficientnet' in Model.name.lower():
+		EF = Model
+	if 'timm_vision_transformer' in Model.name.lower():
+		VT = Model
+ef = EF()
+input_shapes = list(ef.cfg.infer_example_inputs.shape)
+model = ef.model.eval().cuda()
+scripted_model = torch.jit.script(model)
+
+compile_settings = {
+"input_shapes": [input_shapes],
+"op_precision": torch.float16
+}
+
+trt_ts_module = trtorch.compile(scripted_model, compile_settings)
+torch.jit.save(trt_ts_module, 'timm_efficientnet.jit')
+
+ef = VT()
+input_shapes = list(vt.cfg.infer_example_inputs.shape)
+model = vt.model.eval().cuda()
+scripted_model = torch.jit.script(model)
+
+compile_settings = {
+"input_shapes": [input_shapes],
+"op_precision": torch.float16
+}
+
+trt_ts_module = trtorch.compile(scripted_model, compile_settings)
+torch.jit.save(trt_ts_module, 'timm_vision_transformer.jit')
