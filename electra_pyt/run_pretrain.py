359ea861f148cef2d02fe357cf85041ab3ade45e
riship <riship@nvidia.com>
2021-06-03 14:55:09 -0700

    electra

--- electra_pyt/run_pretrain.py
+++ electra_pyt/run_pretrain.py
@@ -289,7 +289,7 @@ class PretrainingModel(nn.Module):
 			logits = pretrain_utils.gather_positions(
 				logits, inputs.masked_lm_positions)
 		oh_labels = torch.zeros(list(inputs.masked_lm_ids.size()) + [self.disc_config.vocab_size], device=torch.cuda.current_device())
-		oh_labels.scatter_(1, inputs.masked_lm_ids, 1)
+		oh_labels.scatter_(1, inputs.masked_lm_ids.reshape(-1,1), 1)
 		# oh_labels = F.one_hot(, num_classes=self.disc_config.vocab_size).type()
 
 		probs = F.softmax(logits, dim=-1)
