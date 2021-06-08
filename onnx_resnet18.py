e7f4fe26bca666bb92c6fa4020cda8701ce7bd88
riship <riship@nvidia.com>
2021-06-08 14:26:35 -0700

    nvfuser on

--- onnx_resnet18.py
+++ onnx_resnet18.py
@@ -18,25 +18,25 @@ with trt.Builder(TRT_LOGGER) as builder, builder.create_builder_config() as conf
 	with open(ONNX_MODEL, 'rb') as model:
 		parser.parse(model.read())
 	# Build and return the engine. Note that the builder, network and parser are destroyed when this function returns.
-	with builder.build_engine(network, config) as engine:
-		with engine.create_execution_context() as context:
-				since=time.time()
-				for i in range(10):
-					# Determine dimensions and create page-locked memory buffers (i.e. won't be swapped to disk) to hold host inputs/outputs.
-					h_input = cuda.pagelocked_empty(trt.volume(context.get_binding_shape(0)), dtype=np.float32)
-					h_output = cuda.pagelocked_empty(trt.volume(context.get_binding_shape(1)), dtype=np.float32)
-					# Allocate device memory for inputs and outputs.
-					d_input = cuda.mem_alloc(h_input.nbytes)
-					d_output = cuda.mem_alloc(h_output.nbytes)
-					# Create a stream in which to copy inputs/outputs and run inference.
-					stream = cuda.Stream()
-					# Transfer input data to the GPU.
-					cuda.memcpy_htod_async(d_input, h_input, stream)
-					# Run inference.
-					context.execute_async_v2(bindings=[int(d_input), int(d_output)], stream_handle=stream.handle)
-					# Transfer predictions back from the GPU.
-					cuda.memcpy_dtoh_async(h_output, d_output, stream)
-					# Synchronize the stream
-					stream.synchronize()
+	engine=builder.build_engine(network, config)
+	context=engine.create_execution_context()
+	since=time.time()
+	for i in range(10):
+		# Determine dimensions and create page-locked memory buffers (i.e. won't be swapped to disk) to hold host inputs/outputs.
+		h_input = cuda.pagelocked_empty(trt.volume(context.get_binding_shape(0)), dtype=np.float32)
+		h_output = cuda.pagelocked_empty(trt.volume(context.get_binding_shape(1)), dtype=np.float32)
+		# Allocate device memory for inputs and outputs.
+		d_input = cuda.mem_alloc(h_input.nbytes)
+		d_output = cuda.mem_alloc(h_output.nbytes)
+		# Create a stream in which to copy inputs/outputs and run inference.
+		stream = cuda.Stream()
+		# Transfer input data to the GPU.
+		cuda.memcpy_htod_async(d_input, h_input, stream)
+		# Run inference.
+		context.execute_async_v2(bindings=[int(d_input), int(d_output)], stream_handle=stream.handle)
+		# Transfer predictions back from the GPU.
+		cuda.memcpy_dtoh_async(h_output, d_output, stream)
+		# Synchronize the stream
+		stream.synchronize()
 print("Avg iter time:",round(time.time()-since,2),"seconds")
 				
