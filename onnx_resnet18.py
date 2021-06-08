dcc829cb113911993ef703ccc8cd334b81530085
riship <riship@nvidia.com>
2021-06-08 11:57:52 -0700

    nvfuser on

--- onnx_resnet18.py
+++ onnx_resnet18.py
@@ -2,5 +2,5 @@ import torch
 import torchvision.models as models
 
 dummy_input = torch.randn(1, 3, 224, 224, device='cuda')
-model = models.resnet18()
+model = models.resnet18().cuda()
 torch.onnx.export(model, dummy_input, "res18.onnx", verbose=True)
