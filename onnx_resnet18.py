97bf49c61651a1e36fa85105e720793d5543742a
riship <riship@nvidia.com>
2021-06-08 14:58:53 -0700

    nvfuser on

--- onnx_resnet18.py
+++ onnx_resnet18.py
@@ -7,25 +7,17 @@ import pycuda.driver as cuda
 import time
 import tensorrt as trt
 
-dummy_input = torch.randn(1, 3, 224, 224).cuda()
-model = models.resnet18().cuda()
-torch.onnx.export(model, dummy_input, "res18.onnx", verbose=False)
 
-TRT_LOGGER = trt.Logger(trt.Logger.WARNING)
-model_path ="res18.onnx"
-input_size = 224
-TRT_LOGGER = trt.Logger(trt.Logger.WARNING)
-EXPLICIT_BATCH = 1 << (int)(trt.NetworkDefinitionCreationFlag.EXPLICIT_BATCH)
 
 
 def build_engine(model_path):
-    with trt.Builder(TRT_LOGGER) as builder,builder.create_network(EXPLICIT_BATCH) as network,trt.OnnxParser(network, TRT_LOGGER) as parser, builder.create_builder_config() as config:
-        config.max_workspace_size = 1<<30
-        # config.max_batch_size = 1
-        with open(model_path, "rb") as f:
-            parser.parse(f.read())
-        engine = builder.build_engine(network, config)
-    return engine
+	with trt.Builder(TRT_LOGGER) as builder,builder.create_network(EXPLICIT_BATCH) as network,trt.OnnxParser(network, TRT_LOGGER) as parser, builder.create_builder_config() as config:
+		config.max_workspace_size = 1<<30
+		# config.max_batch_size = 1
+		with open(model_path, "rb") as f:
+			parser.parse(f.read())
+		engine = builder.build_engine(network, config)
+	return engine
 # def inference(engine, context, inputs, out_cpu, in_gpu, out_gpu, stream):
 # async version
 # with engine.create_execution_context() as context:  # cost time to initialize
@@ -34,61 +26,69 @@ def build_engine(model_path):
 # cuda.memcpy_dtoh_async(out_cpu, out_gpu, stream)
 # stream.synchronize()
 def inference(engine, context, inputs, h_input, h_output, d_input, d_output, stream):
-    cuda.memcpy_htod_async(d_input, h_input, stream)
-    # Run inference.
-    context.execute_async(bindings=[int(d_input), int(d_output)], stream_handle=stream.handle)
-    # Transfer predictions back from the GPU.
-    cuda.memcpy_dtoh_async(h_output, d_output, stream)
-    return h_output
+	cuda.memcpy_htod_async(d_input, h_input, stream)
+	# Run inference.
+	context.execute_async(bindings=[int(d_input), int(d_output)], stream_handle=stream.handle)
+	# Transfer predictions back from the GPU.
+	cuda.memcpy_dtoh_async(h_output, d_output, stream)
+	return h_output
 
-    '''
-    # sync version
-    cuda.memcpy_htod(in_gpu, inputs,stream)
-    context.execute(1, [int(in_gpu), int(out_gpu)])
-    cuda.memcpy_dtoh(out_cpu, out_gpu,stream)
-    return out_cpu'''
+	'''
+	# sync version
+	cuda.memcpy_htod(in_gpu, inputs,stream)
+	context.execute(1, [int(in_gpu), int(out_gpu)])
+	cuda.memcpy_dtoh(out_cpu, out_gpu,stream)
+	return out_cpu'''
 
 
 def alloc_buf(engine):
-    h_input = cuda.pagelocked_empty(trt.volume(engine.get_binding_shape(0)), dtype=np.float32)
-    h_output = cuda.pagelocked_empty(trt.volume(engine.get_binding_shape(1)), dtype=np.float32)
-    # Allocate device memory for inputs and outputs.
-    d_input = cuda.mem_alloc(h_input.nbytes)
-    d_output = cuda.mem_alloc(h_output.nbytes)
-    stream = cuda.Stream()
+	h_input = cuda.pagelocked_empty(trt.volume(engine.get_binding_shape(0)), dtype=np.float32)
+	h_output = cuda.pagelocked_empty(trt.volume(engine.get_binding_shape(1)), dtype=np.float32)
+	# Allocate device memory for inputs and outputs.
+	d_input = cuda.mem_alloc(h_input.nbytes)
+	d_output = cuda.mem_alloc(h_output.nbytes)
+	stream = cuda.Stream()
 
-    return h_input, h_output, d_input, d_output, stream
+	return h_input, h_output, d_input, d_output, stream
 
 
 if __name__ == "__main__":
-    for i in range(6):
-        if (i < 3):
-            inputs = np.random.random((1, 3, input_size, input_size)).astype(np.float32)
-            engine = build_engine(model_path)
-            print("Engine Created :", type(engine))
-            context = engine.create_execution_context()
-            print("Context executed ", type(context))
-            serialized_engine = engine.serialize()
-            t1 = time.time()
-            # in_cpu, out_cpu, in_gpu, out_gpu, stream = alloc_buf(engine)
-            h_input, h_output, d_input, d_output, stream = alloc_buf(engine)
-            res = inference(engine, context, inputs.reshape(-1), h_input, h_output, d_input, d_output, stream)
-            # print(type(res))
-            print("using fp32 mode:")
-            print("cost time: ", time.time() - t1)
-        if (i >= 3):
-            inputs = np.random.random((1, 3, input_size, input_size)).astype(np.float16)
-            engine = build_engine(model_path)
-            print("Engine Created :", type(engine))
-            context = engine.create_execution_context()
-            print("Context executed ", type(context))
-            serialized_engine = engine.serialize()
-            t1 = time.time()
-            # in_cpu, out_cpu, in_gpu, out_gpu, stream = alloc_buf(engine)
-            h_input, h_output, d_input, d_output, stream = alloc_buf(engine)
-            res = inference(engine, context, inputs.reshape(-1), h_input, h_output, d_input, d_output, stream)
-            print(type(res))
-            print("using fp16 mode:")
-            print("cost time: ", time.time() - t1)
+	dummy_input = torch.randn(1, 3, 224, 224).cuda()
+	model = models.resnet18().cuda()
+	torch.onnx.export(model, dummy_input, "res18.onnx", verbose=False)
+
+	TRT_LOGGER = trt.Logger(trt.Logger.WARNING)
+	model_path ="res18.onnx"
+	input_size = 224
+	TRT_LOGGER = trt.Logger(trt.Logger.WARNING)
+	EXPLICIT_BATCH = 1 << (int)(trt.NetworkDefinitionCreationFlag.EXPLICIT_BATCH)
+	engine = build_engine(model_path)
+	print("Engine Created :", type(engine))
+	context = engine.create_execution_context()
+	print("Context executed ", type(context))
+	for i in range(5):
+		inputs = np.random.random((1, 3, input_size, input_size)).astype(np.float32)
+		t1 = time.time()
+		# in_cpu, out_cpu, in_gpu, out_gpu, stream = alloc_buf(engine)
+		h_input, h_output, d_input, d_output, stream = alloc_buf(engine)
+		res = inference(engine, context, inputs.reshape(-1), h_input, h_output, d_input, d_output, stream)
+		# print(type(res))
+		
+		time_sum+=time.time()-t1
+	print("using fp32 mode:")
+	print("avg cost time: ", round(1000*time_sum/(i+1),4))
+	time_sum=0
+	for i in range(5):
+		inputs = np.random.random((1, 3, input_size, input_size)).astype(np.float16)
+		t1 = time.time()
+		# in_cpu, out_cpu, in_gpu, out_gpu, stream = alloc_buf(engine)
+		h_input, h_output, d_input, d_output, stream = alloc_buf(engine)
+		res = inference(engine, context, inputs.reshape(-1), h_input, h_output, d_input, d_output, stream)
+		# print(type(res))
+		
+		time_sum+=time.time()-t1
+	print("using fp16 mode:")
+	print("avg cost time: ", round(1000*time_sum/(i+1),4))
+
 
 				
