c30fb54e48e67168efe5c88310c6ed8199bf669c
riship <riship@nvidia.com>
2021-06-10 10:48:07 -0700

    updating

--- onnx_ef.py
+++ onnx_ef.py
@@ -92,6 +92,10 @@ if __name__ == "__main__":
 		time_sum+=time.time()-t1
 	print("using onnxTRT fp16 mode:")
 	print("avg cost time: ", round(1000*time_sum/(i+1),4),'ms')
+
+
+
+
 	time_sum=0
 	model1=model.float().eval()
 	for i in range(5):
@@ -223,4 +227,59 @@ if __name__ == "__main__":
 		
 		time_sum+=time.time()-t1
 	print("using nvfusedcudagraphsfp16 mode:")
+	print("avg cost time: ", round(1000.0*time_sum/5.0,4),'ms')
+
+
+	time_sum=0
+	torch.backends.cudnn.benchmark = True
+	s = torch.cuda.Stream()
+	torch.cuda.synchronize()
+	inputs = np.random.random((1, 3, input_size, input_size)).astype(np.float32)
+	with torch.cuda.stream(s):
+		for _ in range(5):
+			h_input, h_output, d_input, d_output, stream = alloc_buf(engine, np.float32)
+			res = inference(engine, context, inputs.reshape(-1), h_input, h_output, d_input, d_output, stream)
+		torch.cuda.empty_cache()
+		g = torch.cuda._Graph()
+		torch.cuda.synchronize()
+		g.capture_begin()
+		h_input, h_output, d_input, d_output, stream = alloc_buf(engine, np.float32)
+		res = inference(engine, context, inputs.reshape(-1), h_input, h_output, d_input, d_output, stream)
+		g.capture_end()
+		torch.cuda.synchronize()
+
+	for _ in range(5):
+		t1=time.time()
+		g.replay()
+		torch.cuda.synchronize()
+		
+		time_sum+=time.time()-t1
+	print("using torchcudagraphsonnxTRT fp16 mode:")
+	print("avg cost time: ", round(1000.0*time_sum/5.0,4),'ms')
+	#fp16 cudagraphsonnxTRT
+	time_sum=0
+	torch.backends.cudnn.benchmark = True
+	s = torch.cuda.Stream()
+	torch.cuda.synchronize()
+	inputs = np.random.random((1, 3, input_size, input_size)).astype(np.float16)
+	with torch.cuda.stream(s):
+		for _ in range(5):
+			h_input, h_output, d_input, d_output, stream = alloc_buf(engine, np.float16)
+			res = inference(engine, context, inputs.reshape(-1), h_input, h_output, d_input, d_output, stream)
+		torch.cuda.empty_cache()
+		g = torch.cuda._Graph()
+		torch.cuda.synchronize()
+		g.capture_begin()
+		h_input, h_output, d_input, d_output, stream = alloc_buf(engine, np.float32)
+		res = inference(engine, context, inputs.reshape(-1), h_input, h_output, d_input, d_output, stream)
+		g.capture_end()
+		torch.cuda.synchronize()
+
+	for _ in range(5):
+		t1=time.time()
+		g.replay()
+		torch.cuda.synchronize()
+		
+		time_sum+=time.time()-t1
+	print("using torchcudagraphsonnxTRT fp16 mode:")
 	print("avg cost time: ", round(1000.0*time_sum/5.0,4),'ms')
