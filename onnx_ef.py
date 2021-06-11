9ac34bd0e973897d22fc033d70a92acdefbe4d23
riship <riship@nvidia.com>
2021-06-11 11:09:10 -0700

    updating

--- onnx_ef.py
+++ onnx_ef.py
@@ -6,6 +6,7 @@ import pycuda.autoinit
 import pycuda.driver as cuda
 import time
 import tensorrt as trt
+import torch.cuda.nvtx as nvtx
 
 
 
@@ -68,168 +69,169 @@ if __name__ == "__main__":
 	print("Engine Created :", type(engine))
 	context = engine.create_execution_context()
 	print("Context executed ", type(context))
-	h_input, h_output, d_input, d_output, stream = alloc_buf(engine, np.float32)
-	time_sum=0
-	for i in range(5):
-		inputs = np.random.random((1, 3, input_size, input_size)).astype(np.float32)
-		t1 = time.time()
-		# in_cpu, out_cpu, in_gpu, out_gpu, stream = alloc_buf(engine)
+
+	# h_input, h_output, d_input, d_output, stream = alloc_buf(engine, np.float32)
+	# time_sum=0
+	# for i in range(5):
+	# 	inputs = np.random.random((1, 3, input_size, input_size)).astype(np.float32)
+	# 	t1 = time.time()
+	# 	# in_cpu, out_cpu, in_gpu, out_gpu, stream = alloc_buf(engine)
 		
-		res = inference(engine, context, inputs.reshape(-1), h_input, h_output, d_input, d_output, stream)
-		# print(type(res))
+	# 	res = inference(engine, context, inputs.reshape(-1), h_input, h_output, d_input, d_output, stream)
+	# 	# print(type(res))
 		
-		time_sum+=time.time()-t1
-	print("using onnxTRT fp32 mode:")
-	print("avg cost time: ", round(1000.0*time_sum/5.0,4),'ms')
-	time_sum=0
-	h_input, h_output, d_input, d_output, stream = alloc_buf(engine,np.float16)
-	for i in range(5):
-		inputs = np.random.random((1, 3, input_size, input_size)).astype(np.float16)
-		t1 = time.time()
-		# in_cpu, out_cpu, in_gpu, out_gpu, stream = alloc_buf(engine)
+	# 	time_sum+=time.time()-t1
+	# print("using onnxTRT fp32 mode:")
+	# print("avg cost time: ", round(1000.0*time_sum/5.0,4),'ms')
+	# time_sum=0
+	# h_input, h_output, d_input, d_output, stream = alloc_buf(engine,np.float16)
+	# for i in range(5):
+	# 	inputs = np.random.random((1, 3, input_size, input_size)).astype(np.float16)
+	# 	t1 = time.time()
+	# 	# in_cpu, out_cpu, in_gpu, out_gpu, stream = alloc_buf(engine)
 		
-		res = inference(engine, context, inputs.reshape(-1), h_input, h_output, d_input, d_output, stream)
-		# print(type(res))
+	# 	res = inference(engine, context, inputs.reshape(-1), h_input, h_output, d_input, d_output, stream)
+	# 	# print(type(res))
 		
-		time_sum+=time.time()-t1
-	print("using onnxTRT fp16 mode:")
-	print("avg cost time: ", round(1000*time_sum/(i+1),4),'ms')
+	# 	time_sum+=time.time()-t1
+	# print("using onnxTRT fp16 mode:")
+	# print("avg cost time: ", round(1000*time_sum/(i+1),4),'ms')
 
 
 
 
-	time_sum=0
-	model1=model.float().eval()
-	for i in range(5):
-		inputs = torch.tensor(np.random.random((1, 3, input_size, input_size))).cuda().float()
-		t1 = time.time()
-		# in_cpu, out_cpu, in_gpu, out_gpu, stream = alloc_buf(engine)
-		out=model1(inputs)
-		# print(type(res))
+	# time_sum=0
+	# model1=model.float().eval()
+	# for i in range(5):
+	# 	inputs = torch.tensor(np.random.random((1, 3, input_size, input_size))).cuda().float()
+	# 	t1 = time.time()
+	# 	# in_cpu, out_cpu, in_gpu, out_gpu, stream = alloc_buf(engine)
+	# 	out=model1(inputs)
+	# 	# print(type(res))
 		
-		time_sum+=time.time()-t1
-	print("using torch fp32 mode:")
-	print("avg cost time: ", round(1000.0*time_sum/5.0,4),'ms')
-	time_sum=0
-	halfmodel=model.half().eval()
-	for i in range(5):
-		inputs = torch.tensor(np.random.random((1, 3, input_size, input_size))).cuda().half()
+	# 	time_sum+=time.time()-t1
+	# print("using torch fp32 mode:")
+	# print("avg cost time: ", round(1000.0*time_sum/5.0,4),'ms')
+	# time_sum=0
+	# halfmodel=model.half().eval()
+	# for i in range(5):
+	# 	inputs = torch.tensor(np.random.random((1, 3, input_size, input_size))).cuda().half()
 		
-		t1 = time.time()
-		# in_cpu, out_cpu, in_gpu, out_gpu, stream = alloc_buf(engine)
-		out=halfmodel(inputs)
-		# print(type(res))
+	# 	t1 = time.time()
+	# 	# in_cpu, out_cpu, in_gpu, out_gpu, stream = alloc_buf(engine)
+	# 	out=halfmodel(inputs)
+	# 	# print(type(res))
 		
-		time_sum+=time.time()-t1
-	print("using torch fp16 mode:")
-	print("avg cost time: ", round(1000.0*time_sum/5.0,4),'ms')
-	time_sum=0
-	torch.backends.cudnn.benchmark = True
-	model1=model.float().eval()
-	s = torch.cuda.Stream()
-	torch.cuda.synchronize()
-	inputs = torch.tensor(np.random.random((1, 3, input_size, input_size))).cuda().float()
-	with torch.cuda.stream(s):
-		for _ in range(5):
-			out=model1(inputs)
-		torch.cuda.empty_cache()
-		g = torch.cuda._Graph()
-		torch.cuda.synchronize()
-		g.capture_begin()
-		out=model1(inputs)
-		g.capture_end()
-		torch.cuda.synchronize()
+	# 	time_sum+=time.time()-t1
+	# print("using torch fp16 mode:")
+	# print("avg cost time: ", round(1000.0*time_sum/5.0,4),'ms')
+	# time_sum=0
+	# torch.backends.cudnn.benchmark = True
+	# model1=model.float().eval()
+	# s = torch.cuda.Stream()
+	# torch.cuda.synchronize()
+	# inputs = torch.tensor(np.random.random((1, 3, input_size, input_size))).cuda().float()
+	# with torch.cuda.stream(s):
+	# 	for _ in range(5):
+	# 		out=model1(inputs)
+	# 	torch.cuda.empty_cache()
+	# 	g = torch.cuda._Graph()
+	# 	torch.cuda.synchronize()
+	# 	g.capture_begin()
+	# 	out=model1(inputs)
+	# 	g.capture_end()
+	# 	torch.cuda.synchronize()
 
-	for _ in range(5):
-		t1=time.time()
-		g.replay()
-		torch.cuda.synchronize()
+	# for _ in range(5):
+	# 	t1=time.time()
+	# 	g.replay()
+	# 	torch.cuda.synchronize()
 		
-		time_sum+=time.time()-t1
-	print("using cudagraphsfp32 mode:")
-	print("avg cost time: ", round(1000.0*time_sum/5.0,4),'ms')
-	time_sum=0
-	torch.backends.cudnn.benchmark = True
-	model2=model.half().eval()
-	s = torch.cuda.Stream()
-	torch.cuda.synchronize()
-	inputs = torch.tensor(np.random.random((1, 3, input_size, input_size))).cuda().half()
-	with torch.cuda.stream(s):
-		for _ in range(5):
-			out=model2(inputs)
-		torch.cuda.empty_cache()
-		g = torch.cuda._Graph()
-		torch.cuda.synchronize()
-		g.capture_begin()
-		out=model2(inputs)
-		g.capture_end()
-		torch.cuda.synchronize()
+	# 	time_sum+=time.time()-t1
+	# print("using cudagraphsfp32 mode:")
+	# print("avg cost time: ", round(1000.0*time_sum/5.0,4),'ms')
+	# time_sum=0
+	# torch.backends.cudnn.benchmark = True
+	# model2=model.half().eval()
+	# s = torch.cuda.Stream()
+	# torch.cuda.synchronize()
+	# inputs = torch.tensor(np.random.random((1, 3, input_size, input_size))).cuda().half()
+	# with torch.cuda.stream(s):
+	# 	for _ in range(5):
+	# 		out=model2(inputs)
+	# 	torch.cuda.empty_cache()
+	# 	g = torch.cuda._Graph()
+	# 	torch.cuda.synchronize()
+	# 	g.capture_begin()
+	# 	out=model2(inputs)
+	# 	g.capture_end()
+	# 	torch.cuda.synchronize()
 
-	for _ in range(5):
-		t1=time.time()
-		g.replay()
-		torch.cuda.synchronize()
+	# for _ in range(5):
+	# 	t1=time.time()
+	# 	g.replay()
+	# 	torch.cuda.synchronize()
 		
-		time_sum+=time.time()-t1
-	print("using cudagraphsfp16 mode:")
-	print("avg cost time: ", round(1000.0*time_sum/5.0,4),'ms')
-	torch._C._jit_set_nvfuser_enabled(True)
-	torch._C._jit_set_texpr_fuser_enabled(False)
-	torch._C._jit_set_profiling_executor(True)
-	torch._C._jit_set_profiling_mode(True)
-	torch._C._jit_override_can_fuse_on_cpu(False)
-	torch._C._jit_override_can_fuse_on_gpu(False)
-	torch._C._jit_set_bailout_depth(20)
-	time_sum=0
-	torch.backends.cudnn.benchmark = True
-	model1=torch.jit.script(timm.create_model('mixnet_m', pretrained=False, scriptable=True).cuda()).float().eval()
-	s = torch.cuda.Stream()
-	torch.cuda.synchronize()
-	inputs = torch.tensor(np.random.random((1, 3, input_size, input_size))).cuda().float()
-	with torch.cuda.stream(s):
-		for _ in range(5):
-			out=model1(inputs)
-		torch.cuda.empty_cache()
-		g = torch.cuda._Graph()
-		torch.cuda.synchronize()
-		g.capture_begin()
-		out=model1(inputs)
-		g.capture_end()
-		torch.cuda.synchronize()
+	# 	time_sum+=time.time()-t1
+	# print("using cudagraphsfp16 mode:")
+	# print("avg cost time: ", round(1000.0*time_sum/5.0,4),'ms')
+	# torch._C._jit_set_nvfuser_enabled(True)
+	# torch._C._jit_set_texpr_fuser_enabled(False)
+	# torch._C._jit_set_profiling_executor(True)
+	# torch._C._jit_set_profiling_mode(True)
+	# torch._C._jit_override_can_fuse_on_cpu(False)
+	# torch._C._jit_override_can_fuse_on_gpu(False)
+	# torch._C._jit_set_bailout_depth(20)
+	# time_sum=0
+	# torch.backends.cudnn.benchmark = True
+	# model1=torch.jit.script(timm.create_model('mixnet_m', pretrained=False, scriptable=True).cuda()).float().eval()
+	# s = torch.cuda.Stream()
+	# torch.cuda.synchronize()
+	# inputs = torch.tensor(np.random.random((1, 3, input_size, input_size))).cuda().float()
+	# with torch.cuda.stream(s):
+	# 	for _ in range(5):
+	# 		out=model1(inputs)
+	# 	torch.cuda.empty_cache()
+	# 	g = torch.cuda._Graph()
+	# 	torch.cuda.synchronize()
+	# 	g.capture_begin()
+	# 	out=model1(inputs)
+	# 	g.capture_end()
+	# 	torch.cuda.synchronize()
 
-	for _ in range(5):
-		t1=time.time()
-		g.replay()
-		torch.cuda.synchronize()
+	# for _ in range(5):
+	# 	t1=time.time()
+	# 	g.replay()
+	# 	torch.cuda.synchronize()
 		
-		time_sum+=time.time()-t1
-	print("using nvfusedcudagraphsfp32 mode:")
-	print("avg cost time: ", round(1000.0*time_sum/5.0,4),'ms')
-	time_sum=0
-	torch.backends.cudnn.benchmark = True
-	model2=torch.jit.script(timm.create_model('mixnet_m', pretrained=False, scriptable=True).cuda()).half().eval()
-	s = torch.cuda.Stream()
-	torch.cuda.synchronize()
-	inputs = torch.tensor(np.random.random((1, 3, input_size, input_size))).cuda().half()
-	with torch.cuda.stream(s):
-		for _ in range(5):
-			out=model2(inputs)
-		torch.cuda.empty_cache()
-		g = torch.cuda._Graph()
-		torch.cuda.synchronize()
-		g.capture_begin()
-		out=model2(inputs)
-		g.capture_end()
-		torch.cuda.synchronize()
+	# 	time_sum+=time.time()-t1
+	# print("using nvfusedcudagraphsfp32 mode:")
+	# print("avg cost time: ", round(1000.0*time_sum/5.0,4),'ms')
+	# time_sum=0
+	# torch.backends.cudnn.benchmark = True
+	# model2=torch.jit.script(timm.create_model('mixnet_m', pretrained=False, scriptable=True).cuda()).half().eval()
+	# s = torch.cuda.Stream()
+	# torch.cuda.synchronize()
+	# inputs = torch.tensor(np.random.random((1, 3, input_size, input_size))).cuda().half()
+	# with torch.cuda.stream(s):
+	# 	for _ in range(5):
+	# 		out=model2(inputs)
+	# 	torch.cuda.empty_cache()
+	# 	g = torch.cuda._Graph()
+	# 	torch.cuda.synchronize()
+	# 	g.capture_begin()
+	# 	out=model2(inputs)
+	# 	g.capture_end()
+	# 	torch.cuda.synchronize()
 
-	for _ in range(5):
-		t1=time.time()
-		g.replay()
-		torch.cuda.synchronize()
+	# for _ in range(5):
+	# 	t1=time.time()
+	# 	g.replay()
+	# 	torch.cuda.synchronize()
 		
-		time_sum+=time.time()-t1
-	print("using nvfusedcudagraphsfp16 mode:")
-	print("avg cost time: ", round(1000.0*time_sum/5.0,4),'ms')
+	# 	time_sum+=time.time()-t1
+	# print("using nvfusedcudagraphsfp16 mode:")
+	# print("avg cost time: ", round(1000.0*time_sum/5.0,4),'ms')
 
 
 
@@ -238,35 +240,43 @@ if __name__ == "__main__":
 	torch.backends.cudnn.benchmark = True
 	s = torch.cuda.Stream()
 	torch.cuda.synchronize()
-	inputs = np.random.random((1, 3, input_size, input_size)).astype(np.float16)
-	allocd=False
+	h_input, h_output, d_input, d_output, stream = alloc_buf(engine, np.float16)
+	oginputs = np.random.random((1, 3, input_size, input_size)).astype(np.float16)
+	nvtx.range_push('warmup and capture')
+
 	with torch.cuda.stream(s):
 		for _ in range(5):
-			if not allocd:
-				h_input, h_output, d_input, d_output, stream = alloc_buf(engine, np.float16)
-				allocd=True
-			res = inference(engine, context, inputs.reshape(-1), h_input, h_output, d_input, d_output, stream)
-		oginputs = np.random.random((1, 3, input_size, input_size)).astype(np.float16)
+			inputs = np.random.random((1, 3, input_size, input_size)).astype(np.float16)
+			oginputs[:] = inputs
+			oginputs[:] = oginputs * 2
+			#res = inference(engine, context, inputs.reshape(-1), h_input, h_output, d_input, d_output, stream)
+		
 		torch.cuda.empty_cache()
 		g = torch.cuda._Graph()
 		torch.cuda.synchronize()
+		inputs = np.random.random((1, 3, input_size, input_size)).astype(np.float16)
+		oginputs[:] = inputs
 		g.capture_begin()
 		# h_input, h_output, d_input, d_output, stream = alloc_buf(engine, np.float32)
-		res = inference(engine, context, oginputs.reshape(-1), h_input, h_output, d_input, d_output, stream)
+		# res = inference(engine, context, oginputs.reshape(-1), h_input, h_output, d_input, d_output, stream)
+		oginputs[:]=oginputs*2
 		g.capture_end()
 		torch.cuda.synchronize()
-
+	nvtx.range_pop()
+	nvtx.range_push("replaying...")
 	for i in range(5):
 		inputs = np.random.random((1, 3, input_size, input_size)).astype(np.float16)
+		nvtx.range_push("Singular Replay")
 		t1=time.time()
 		oginputs[:]=inputs
 		g.replay()
 		torch.cuda.synchronize()
-		
+		nvtx.range_pop()
 		time_sum+=time.time()-t1
 		if i!=0:
 			if (previous_out == h_output).all():
 				print("inputs are changing but outputs are not")
 		previous_out=h_output.copy()
+	nvtx.range_pop()
 	print("using torchcudagraphsonnxTRT fp16 mode:")
 	print("avg cost time: ", round(1000.0*time_sum/5.0,4),'ms')
