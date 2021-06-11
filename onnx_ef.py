f16475bf3f2a059d501a16ae39fff02433488aaa
riship <riship@nvidia.com>
2021-06-11 12:25:13 -0700

    updating

--- onnx_ef.py
+++ onnx_ef.py
@@ -56,85 +56,98 @@ import os
 import timm.models.efficientnet
 import timm
 if __name__ == "__main__":
-	if not os.path.exists(os.getcwd()+os.sep+'ef.onnx'):
-		dummy_input = torch.randn(1, 3, 224, 224).cuda()
-		model = timm.create_model('mixnet_m', pretrained=False, scriptable=True).cuda().float()
+	# if not os.path.exists(os.getcwd()+os.sep+'ef.onnx'):
+	# 	dummy_input = torch.randn(1, 3, 224, 224).cuda()
+	# 	model = timm.create_model('mixnet_m', pretrained=False, scriptable=True).cuda().float()
 
-		torch.onnx.export(model, dummy_input, "ef.onnx", verbose=False)
+	# 	torch.onnx.export(model, dummy_input, "ef.onnx", verbose=False)
 
-	TRT_LOGGER = trt.Logger(trt.Logger.WARNING)
-	model_path ="ef.onnx"
-	input_size = 224
-	TRT_LOGGER = trt.Logger(trt.Logger.WARNING)
-	EXPLICIT_BATCH = 1 << (int)(trt.NetworkDefinitionCreationFlag.EXPLICIT_BATCH)
-	engine = build_engine(model_path)
-	print("Engine Created :", type(engine))
-	context = engine.create_execution_context()
-	print("Context executed ", type(context))
+	# TRT_LOGGER = trt.Logger(trt.Logger.WARNING)
+	# model_path ="ef.onnx"
+	# input_size = 224
+	# TRT_LOGGER = trt.Logger(trt.Logger.WARNING)
+	# EXPLICIT_BATCH = 1 << (int)(trt.NetworkDefinitionCreationFlag.EXPLICIT_BATCH)
+	# engine = build_engine(model_path)
+	# print("Engine Created :", type(engine))
+	# context = engine.create_execution_context()
+	# print("Context executed ", type(context))
 
-	time_sum=0
-	h_input, h_output, d_input, d_output, stream = alloc_buf(engine, np.float32)
-	for i in range(5):
-		inputs = np.random.random((1, 3, input_size, input_size)).astype(np.float32)
-		t1 = time.time()
-		
-
-		res = inference(engine, context, inputs, h_input, h_output, d_input, d_output, stream)
-		# print(type(res))
-		
-		time_sum+=time.time()-t1
-		if i!=0:
-			if (previous_out == h_output).all():
-				print("inputs are changing but outputs are not")
-		previous_out=np.copy(h_output.copy())
-	print("using onnxTRT fp32 mode:")
-	print("avg cost time: ", round(1000.0*time_sum/5.0,4),'ms')
-	# #fp16 cudagraphsonnxTRT
 	# time_sum=0
-	# torch.backends.cudnn.benchmark = True
-	# s = torch.cuda.Stream()
-	# torch.cuda.synchronize()
-	# h_input, h_output, d_input, d_output, stream = alloc_buf(engine, np.float16)
-	# oginputs = np.random.random((1, 3, input_size, input_size)).astype(np.float16)
-	# ogoutputs = np.random.random((1, 3, input_size, input_size)).astype(np.float16)
-	# nvtx.range_push('warmup and capture')
-
-	# with torch.cuda.stream(s):
-	# 	for _ in range(5):
-	# 		inputs = np.random.random((1, 3, input_size, input_size)).astype(np.float16)
-	# 		oginputs[:] = inputs
-	# 		ogoutputs[:] = oginputs * 2
-	# 		#res = inference(engine, context, inputs, h_input, h_output, d_input, d_output, stream)
-		
-	# 	torch.cuda.empty_cache()
-	# 	g = torch.cuda._Graph()
-	# 	torch.cuda.synchronize()
-	# 	inputs = np.random.random((1, 3, input_size, input_size)).astype(np.float16)
-	# 	oginputs[:] = inputs
-	# 	g.capture_begin()
-	# 	# h_input, h_output, d_input, d_output, stream = alloc_buf(engine, np.float32)
-	# 	# res = inference(engine, context, oginputs, h_input, h_output, d_input, d_output, stream)
-	# 	ogoutputs[:] = oginputs * 2
-	# 	g.capture_end()
-	# 	torch.cuda.synchronize()
-	# nvtx.range_pop()
-	# nvtx.range_push("replaying...")
+	# h_input, h_output, d_input, d_output, stream = alloc_buf(engine, np.float32)
 	# for i in range(5):
-	# 	inputs = np.random.random((1, 3, input_size, input_size)).astype(np.float16)
-	# 	nvtx.range_push("Singular Replay")
-	# 	t1=time.time()
-	# 	oginputs[:]=inputs
-	# 	g.replay()
-	# 	torch.cuda.synchronize()
-	# 	nvtx.range_pop()
+	# 	inputs = np.random.random((1, 3, input_size, input_size)).astype(np.float32)
+	# 	t1 = time.time()
+		
+
+	# 	res = inference(engine, context, inputs, h_input, h_output, d_input, d_output, stream)
+	# 	# print(type(res))
+		
 	# 	time_sum+=time.time()-t1
 	# 	if i!=0:
-	# 		if (previous_out == ogoutputs).all():
+	# 		if (previous_out == h_output).all():
 	# 			print("inputs are changing but outputs are not")
-	# 	previous_out=np.copy(ogoutputs.copy())
-	# nvtx.range_pop()
-	# print("using torchcudagraphsonnxTRT fp16 mode:")
+	# 	previous_out=np.copy(h_output.copy())
+	# print("using onnxTRT fp32 mode:")
 	# print("avg cost time: ", round(1000.0*time_sum/5.0,4),'ms')
+
+	#fp16 cudagraphsonnxTRT
+	time_sum=0
+	torch.backends.cudnn.benchmark = True
+	s = torch.cuda.Stream()
+	torch.cuda.synchronize()
+	h_input, h_output, d_input, d_output, stream = alloc_buf(engine, np.float16)
+	# oginputs = np.random.random((1, 3, input_size, input_size)).astype(np.float16)
+	# ogoutputs = np.random.random((1, 3, input_size, input_size)).astype(np.float16)
+	oginputs = torch.randn((1, 3, input_size, input_size)).cuda().half()
+	ogoutputs = torch.randn((1, 3, input_size, input_size)).cuda().half()
+	nvtx.range_push('warmup and capture')
+
+	with torch.cuda.stream(s):
+		for _ in range(5):
+			# inputs = np.random.random((1, 3, input_size, input_size)).astype(np.float16)
+			inputs = torch.randn((1, 3, input_size, input_size)).cuda().half()
+			# oginputs[:] = inputs
+			# ogoutputs[:] = oginputs * 2
+			oginputs.copy_(inputs)
+			ogoutputs.copy_(oginputs * 2)
+			#res = inference(engine, context, inputs, h_input, h_output, d_input, d_output, stream)
+		
+		torch.cuda.empty_cache()
+		g = torch.cuda._Graph()
+		torch.cuda.synchronize()
+		# inputs = np.random.random((1, 3, input_size, input_size)).astype(np.float16)
+		inputs = torch.randn((1, 3, input_size, input_size)).cuda().half()
+		# oginputs[:] = inputs
+		oginputs.copy_(inputs)
+		g.capture_begin()
+		# h_input, h_output, d_input, d_output, stream = alloc_buf(engine, np.float32)
+		# res = inference(engine, context, oginputs, h_input, h_output, d_input, d_output, stream)
+		# ogoutputs[:] = oginputs * 2
+		ogoutputs.copy_(oginputs * 2)
+		g.capture_end()
+		torch.cuda.synchronize()
+	nvtx.range_pop()
+	nvtx.range_push("replaying...")
+	for i in range(5):
+		inputs = np.random.random((1, 3, input_size, input_size)).astype(np.float16)
+		nvtx.range_push("Singular Replay")
+		t1=time.time()
+		# oginputs[:]=inputs
+		oginputs.copy_(inputs)
+		g.replay()
+		torch.cuda.synchronize()
+		nvtx.range_pop()
+		time_sum+=time.time()-t1
+		if i!=0:
+			if (previous_out == ogoutputs).all():
+				print("inputs are changing but outputs are not")
+		previous_out=ogoutputs.clone()
+	nvtx.range_pop()
+	print("using torchcudagraphsonnxTRT fp16 mode:")
+	print("avg cost time: ", round(1000.0*time_sum/5.0,4),'ms')
+
+
+
 	# time_sum=0
 	# h_input, h_output, d_input, d_output, stream = alloc_buf(engine,np.float16)
 	# for i in range(5):
