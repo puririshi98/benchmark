24e5190d04fa3b631c368f19733d4af36699f868
riship <riship@nvidia.com>
2021-06-18 10:49:31 -0700

    updating

--- correctness_test.py
+++ correctness_test.py
@@ -8,15 +8,16 @@ bf=torch.nn.functional.conv2d(input.bfloat16(), weight.bfloat16())
 fp32=torch.nn.functional.conv2d(input.float(), weight.float())
 fp16=torch.nn.functional.conv2d(input.half(), weight.half())
 
-
-
+print("mean(FP32-BF16): ",abs(float(torch.mean(fp32-bf).cpu())))
+print("mean(FP32-FP16): ",abs(float(torch.mean(fp32-fp16).cpu())))
 print("Linear layer correctness test:")
 input=torch.randn((1,224)).cuda()
 weight=torch.randn((224,12)).cuda()
 bf=torch.nn.functional.linear(input.bfloat16(), weight.bfloat16())
 fp32=torch.nn.functional.linear(input.float(), weight.float())
 fp16=torch.nn.functional.linear(input.half(), weight.half())
-
+print("mean(FP32-BF16): ",abs(float(torch.mean(fp32-bf).cpu())))
+print("mean(FP32-FP16): ",abs(float(torch.mean(fp32-fp16).cpu())))
 
 print("softmax correctness test")
 
@@ -25,3 +26,5 @@ bf=torch.nn.functional.softmax(input.bfloat16())
 fp32=torch.nn.functional.softmax(input.float())
 fp16=torch.nn.functional.softmax(input.half())
 
+print("mean(FP32-BF16): ",abs(float(torch.mean(fp32-bf).cpu())))
+print("mean(FP32-FP16): ",abs(float(torch.mean(fp32-fp16).cpu())))
