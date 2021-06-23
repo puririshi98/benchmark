baf831d79d72d4190746e577f72d64391b48d544
riship <riship@nvidia.com>
2021-06-23 13:16:03 -0700

    updating

--- autocast_mode.cpp
+++ autocast_mode.cpp
@@ -15,7 +15,7 @@ bool is_enabled() {
   return !c10::impl::tls_is_dispatch_key_excluded(DispatchKey::AutocastCUDA);
 }
 
-void set_enabled(bool new_enabled, bool bfloat) {
+void set_enabled(bool new_enabled) {
   c10::impl::tls_set_dispatch_key_excluded(DispatchKey::AutocastCUDA, !new_enabled);
 }
 
