baa616efd4737189298d1172472bd5cc038db7d7
riship <riship@nvidia.com>
2021-08-01 10:42:50 -0700

    allreduce test

--- torchbenchmark/models/timm_vision_transformer/allreduce_test.py
+++ torchbenchmark/models/timm_vision_transformer/allreduce_test.py
@@ -4,13 +4,15 @@ import timm.models.vision_transformer
 import timm.models.efficientnet
 import time
 import argparse
+import torch.cuda.nvtx as nvtx
+
 parser = argparse.ArgumentParser(__doc__)
 parser.add_argument("--local_rank",type=int)
 args = parser.parse_args()
 device='cuda'
 precision='float32'
 model_names = ['vit_small_patch16_224', 'mixnet_m']
-world=4
+world=8
 models = [timm.create_model(name, pretrained=False, scriptable=True).cuda().float() for name in model_names]
 torch.distributed.init_process_group("nccl", rank=args.local_rank, world_size=world)
 for model, name in zip(models,model_names):	
@@ -26,5 +28,29 @@ for model, name in zip(models,model_names):
 		for i, t in enumerate(tensors):
 			assert torch.equal(t, torch.full_like(t, world * (i + (world + 1.) / 2.)))
 if args.local_rank == 0:
-	print("Passed test!")
+	print("Passed Correctness test!")
 
+if args.local_rank == 0:
+	print("Running perf test...")
+torch.cuda.cudart().cudaProfilerStart()
+for model, name in zip(models, model_names):	
+	nvtx.range_push("Profiling Model: " + str(name))
+	shapes = [param.size() for param in model.parameters()]
+	sizes = [param.numel() for param in model.parameters()]
+	if args.local_rank == 0:
+		print("Param Shapes for",name+":",shapes)
+		print("Param Sizes for",name+":",sizes)
+	device = torch.device("cuda:%d" % args.local_rank)
+	for shape in shapes:
+		nvtx.range_push("Coalesce")
+		tensors = [torch.full(shape, args.local_rank + 1 + i, device=device, dtype=torch.float) for i in range(5)]
+		torch.distributed.all_reduce_coalesced(tensors)
+		nvtx.range_pop()
+		nvtx.range_push("Flat All Reduce")
+		tensors = [torch.full(shape, args.local_rank + 1 + i, device=device, dtype=torch.float) for i in range(5)]
+		torch.distributed.all_reduce([torch.cat(tensors)])
+		nvtx.range_pop()
+	nvtx.range_pop()
+torch.cuda.cudart().cudaProfilerStop()
+if args.local_rank == 0:
+	print("Finished perf test!")
