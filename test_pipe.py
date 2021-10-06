6912f1baae245e10b07353ed4a6fba862510e6d4
riship <riship@nvidia.com>
2021-10-06 15:44:48 -0700

    Test pipe

--- test_pipe.py
+++ test_pipe.py
@@ -102,7 +102,7 @@ def assign_chunks(modules, n_devices):
 		start_ptr += modules_in_each_chunk
 	return new_Module
 
-def pipe_setup(implementation, model, infer_inputs, n_devices):
+def pipe_setup(implementation, model, infer_inputs, n_devices, model_name):
 	try:
 		ogmodel = model
 		if implementation == 'native':
