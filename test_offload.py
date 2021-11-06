d8413ed4a49563f017b9b32b889690c7714005e2
riship <riship@nvidia.com>
2021-11-06 14:27:31 -0700

    working on looking into offload

--- test_offload.py
+++ test_offload.py
@@ -80,7 +80,7 @@ def main():
 	runtimes = dict((implementation, {}) for implementation in implementations)
 	for implementation in implementations:
 		print("Implementation:", implementation)
-		for n in range(50,10000,50):
+		for n in range(100,2500,100): #xpl can handle at most 2500
 			print("Testing", n,"layers:")
 			runtime = run_offload(n, implementation)
 			runtimes[implementation][str(n)+' layers'] = runtime
